2e8e328ce8d43f4a794609f12b6863f595bbf243
Vinicius Castro <vmrcastro@gmail.com>
2024-11-14 17:18:14 -0300

    Add files via upload

--- databricks_apps/app.py
+++ databricks_apps/app.py
@@ -10,6 +10,8 @@ from keplergl import keplergl
 import os
 from databricks import sql
 from databricks.sdk.core import Config
+from streamlit_keplergl import keplergl_static
+import h3
 
 
 # ----------------------------------------------------------------
@@ -34,18 +36,25 @@ custom_html = """
     }
 </style>
 """
+
+#st.title("Amazonia GeoAI")
+
 # Display the custom HTML
 st.components.v1.html(custom_html)
 
 # Sidebar content
-st.sidebar.header("Sidebar Title")
-st.sidebar.subheader("Subheading")
-st.sidebar.text("Sidebar content goes here.")
+st.sidebar.header("Amazonia Geo AI")
+st.sidebar.subheader("Menu")
+st.sidebar.link_button("Página inicial", "https://luisassuncaoteste-1444828305810485.aws.databricksapps.com/")
+st.sidebar.link_button("Repositório GIT", "https://github.com/Databricks-BR/amazonia_geoai")
+st.sidebar.link_button("Referências", "https://github.com/Databricks-BR/amazonia_geoai?tab=readme-ov-file#refer%C3%AAncias")
+st.sidebar.link_button("Tire suas dúvidas", "https://e2-demo-field-eng.cloud.databricks.com/genie/rooms/01efa2a4a9ec1e5a88ca3a76e18eda63/chats/01efa2a4b12f15e4adf76161b9640120?o=1444828305810485")
+st.sidebar.link_button("Contato", "mailto:amazonia.geoai@databricks.com")
 
 # Main content
-st.title("Main Content")
-st.write("Welcome to my Streamlit app!")
-st.write("This is the main content area.")
+
+#st.write("Welcome to my Streamlit app!")
+#st.write("This is the main content area.")
 
 # ----------------------------------------------------------------
 # BODY PAGE - GRAPH ANALYSIS
@@ -68,81 +77,129 @@ def sqlQuery(query: str) -> pd.DataFrame:
 @st.cache_data(ttl=30)  # only re-query if it's been 30 seconds
 def getData():
 
-    return sqlQuery("select * from amazonia.geo.gold_h3_scores limit 5000")
+    return sqlQuery("select * from amazonia.geo.gold_h3_score limit 5000")
 
 data = getData()
 
-st.header("Geo Analysis - Scores:)")
-col1, col2 = st.columns([3, 1])
-with col1:
-    st.scatter_chart(data=data, height=400, width=700, y="fare_amount", x="trip_distance")
-with col2:
-    st.subheader("Predict fare")
-    pickup = st.text_input("From (zipcode)", value="10003")
-    dropoff = st.text_input("To (zipcode)", value="11238")
-    d = data[(data['pickup_zip'] == int(pickup)) & (data['dropoff_zip'] == int(dropoff))]
-    st.write(f"# **${d['fare_amount'].mean() if len(d) > 0 else 99:.2f}**")
+# st.header("Geo Analysis - Scores:)")
+# col1, col2 = st.columns([3, 1])
+# with col1:
+#     st.scatter_chart(data=data, height=400, width=700, y="fare_amount", x="trip_distance")
+# with col2:
+#     st.subheader("Predict fare")
+#     pickup = st.text_input("From (zipcode)", value="10003")
+#     dropoff = st.text_input("To (zipcode)", value="11238")
+#     d = data[(data['pickup_zip'] == int(pickup)) & (data['dropoff_zip'] == int(dropoff))]
+#     st.write(f"# **${d['fare_amount'].mean() if len(d) > 0 else 99:.2f}**")
 
-st.dataframe(data=data, height=600, use_container_width=True)
 
 # ----------------------------------------------------------------
 # BODY PAGE - GEO ANALYSIS - KEPLER.GL
 # ----------------------------------------------------------------
 
 # load sampleH3Data from csv and setup the metadata
-h3_hex_id_df = pd.read_csv("data/h3_data.csv")
+h3_hex_id_df = sqlQuery("select * from amazonia.geo.gold_h3_score limit 5000")
 h3_hex_id_df.label = "H3 Hexagons V2"
 h3_hex_id_df.id = "h3-hex-id"
 
-st.subheader("Streamlit Kepler Bidirectional Communication Demo")
+st.subheader("Monitoramento das áreas de risco")
 
-if "datasets" not in st.session_state:
-    st.session_state.datasets = []
 
-options = {"keepExistingConfig": True}
+config = {}
 
-map_config = keplergl(st.session_state.datasets, options=options, config=None, height=400)
-time.sleep(1.5)
-session_data_ids = []
-if map_config:
-    map_config_json = json.loads(map_config)
+config = config = {
+                    "mapStyle": {
+                        "styleType": "satellite",
+                        "topLayerGroups": {},
+                        "visibleLayerGroups": {
+                            "label": True,
+                            "road": True,
+                            "border": False,
+                            "building": True,
+                            "water": True,
+                            "land": True,
+                        },
+                        "mapStyles": {}
+                    }
+                }
 
-    # check if any datasets were deleted
-    map_data_ids = [layer["config"]["dataId"] for layer in map_config_json["visState"]["layers"]]
-    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
-    indices_to_remove = [i for i, dataset in enumerate(st.session_state.datasets) if
-                         not dataset.id in map_data_ids]
-    for i in reversed(indices_to_remove):
-        del st.session_state.datasets[i]
+# Create a Kepler.gl map and add the data
+map_1 = keplergl.KeplerGl(height=600, config=config)
 
-    session_data_ids = [dataset.id for dataset in st.session_state.datasets]
-    # st.markdown(session_data_ids)
+df = h3_hex_id_df
+df['h3_index'] = df['h3_10_id'].apply(lambda x: hex(int(x)).upper().replace("0X",""))
 
-col1, col2, col3 = st.columns([1, 1, 1])
-with col1:
-    san_diego_button_clicked = st.button('Add Bart Stops Geo', disabled=("bart-stops-geo" in session_data_ids))
-    if san_diego_button_clicked:
-        st.session_state.datasets.append(bart_stops_geo_gdf)
-        st.rerun()
+map_1.add_data(data=df, name="h3")
 
-with col2:
-    bart_button_clicked = st.button('Add SF Zip Geo', disabled=("sf-zip-geo" in session_data_ids))
-    if bart_button_clicked:
-        st.session_state.datasets.append(sf_zip_geo_gdf)
-        st.rerun()
+# Display the map in Streamlit
+keplergl_static(map_1, center_map=True)
 
-with col3:
-    h3_button_clicked = st.button('Add H3 Hexagons V2', disabled=("h3-hex-id" in session_data_ids))
-    if h3_button_clicked:
-        st.session_state.datasets.append(h3_hex_id_df)
-        st.rerun()
+# col1, col2 = st.columns(2)
 
-st.markdown("""
-     The status of the map is displayed at the bottom of the page. You can zoom in/out, pan map, and 
-     watch the map state change. You can also change the color of different layers, delete data, and 
-     watch the map status change. In other words, Streamlit will be notified of any changes in the 
-     map state, and Streamlit can also dynamically add data to the map.
-""")
+# with col2:
+#     st.dataframe(data=data, use_container_width=True, hide_index=True)
 
-if map_config:
-    st.code(json.dumps(map_config_json, indent=4))
+st.scatter_chart(data=data, height=400, width=700, y="impact_score", x="critical_score", color="h3_10_id")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# CÓDIGO ANTIGO
+
+# time.sleep(1.5)
+# session_data_ids = []
+# if map_config:
+#     # map_config_json = map_config.config['config']
+#     map_config_json = map_config.config.get('config')
+
+#     # check if any datasets were deleted
+#     map_data_ids = [layer["config"]["dataId"] for layer in map_config_json.get("visState").get("layers")]
+#     # map_data_ids = []
+#     session_data_ids = [dataset.id for dataset in st.session_state.datasets]
+#     indices_to_remove = [i for i, dataset in enumerate(st.session_state.datasets) if
+#                          not dataset.id in map_data_ids]
+#     for i in reversed(indices_to_remove):
+#         del st.session_state.datasets[i]
+
+#     session_data_ids = [dataset.id for dataset in st.session_state.datasets]
+#     # st.markdown(session_data_ids)
+
+# col1, col2, col3 = st.columns([1, 1, 1])
+# with col1:
+#     san_diego_button_clicked = st.button('Add Bart Stops Geo', disabled=("bart-stops-geo" in session_data_ids))
+#     if san_diego_button_clicked:
+#         st.session_state.datasets.append(h3_hex_id_df)
+#         st.rerun()
+
+# with col2:
+#     bart_button_clicked = st.button('Add SF Zip Geo', disabled=("sf-zip-geo" in session_data_ids))
+#     if bart_button_clicked:
+#         st.session_state.datasets.append(sf_zip_geo_gdf)
+#         st.rerun()
+
+# with col3:
+#     h3_button_clicked = st.button('Add H3 Hexagons V2', disabled=("h3-hex-id" in session_data_ids))
+#     if h3_button_clicked:
+#         st.session_state.datasets.append(h3_hex_id_df)
+#         st.rerun()
+
+# st.markdown("""
+#      The status of the map is displayed at the bottom of the page. You can zoom in/out, pan map, and 
+#      watch the map state change. You can also change the color of different layers, delete data, and 
+#      watch the map status change. In other words, Streamlit will be notified of any changes in the 
+#      map state, and Streamlit can also dynamically add data to the map.
+# """)
+
+# if map_config:
+#     st.code(json.dumps(map_config_json, indent=4))
